5c2fdd1d5ee5090ab5afa11cd7dbfdee956c23e5
adnanmutilovic <126587226+adnanmutilovic@users.noreply.github.com>
2023-06-20 16:56:48 +0200

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { login } from "../api/authApi";
+import { login } from "../auth/authApi";
 
 function Login() {
   const [email, setEmail] = useState("");
